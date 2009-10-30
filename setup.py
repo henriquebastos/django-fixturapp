1557ed08d195ff10ed22502485002ff4b9aff667
Henrique Bastos <henrique@bastos.net>
2009-10-29 22:28:06 -0200

    Added fixture module dependency to setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages = find_packages('src'),
     package_dir = {'': 'src'},
 
-    install_requires = ['setuptools'],
+    install_requires = ['setuptools', 'fixture'],
 
     classifiers = [
         'Development Status :: 4 - Beta',

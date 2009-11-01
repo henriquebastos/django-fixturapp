e4d85f71c363f8502ac3aba5ef63aa86c6bdf782
Henrique Bastos <henrique@bastos.net>
2009-11-01 15:06:08 -0200

    Changed fixturapp_loadall Command base class from BaseCommand to NoArgsCommand

--- src/fixturapp/management/commands/fixturapp_loadall.py
+++ src/fixturapp/management/commands/fixturapp_loadall.py
@@ -1,15 +1,15 @@
 from django.core.management import call_command
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import NoArgsCommand, CommandError
 from fixture import DjangoFixture
 from fixture.style import NamedDataStyle
 
 from fixturapp.management.commands import find_datasets, fill_database
 
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
     help = "Load datasets fixtures from Django apps into database"
 
-    def handle(self, **options):
+    def handle_noargs(self, **options):
         from django.conf import settings
 
         apps = settings.INSTALLED_APPS
--- src/fixturapp/tests/test_loadall.py
+++ src/fixturapp/tests/test_loadall.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.management import load_command_class, call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import NoArgsCommand
 from fixturapp.management.commands import fixturapp_loadall
 from dummyapp.models import Dummy
 from dummyapp.datasets import DummyData
@@ -21,7 +21,7 @@ class FixturappLoadAllTests(TestCase):
 
     def test_command_is_valid_django_command(self):
         """Asserts that fixturapp_loadall command is a valid Django command"""
-        self.failUnless(isinstance(create_command(), BaseCommand))
+        self.failUnless(isinstance(create_command(), NoArgsCommand))
 
     def test_fixture_was_loaded_by_calling_command(self):
         """Check if Dummy fixtures were loaded to the database"""

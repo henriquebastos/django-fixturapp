bb7cd7852ee56db27ea4d99727be38ca6832f863
Henrique Bastos <henrique@bastos.net>
2009-11-02 18:54:59 -0200

    Fixed mistyped url in setup.py and adjusted for PEP8

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "django-fixturapp",
-    version = "1.0",
-    url = 'http://github.com/henriquebastos/django-fixture',
-    license = 'BSD',
-    description = "An utility app that integrates Fixture with Django.",
-    long_description = read('README'),
+    name="django-fixturapp",
+    version="1.0",
+    url='http://github.com/henriquebastos/django-fixturapp',
+    license='BSD',
+    description="An utility app that integrates Fixture with Django.",
+    long_description=read('README'),
 
-    author = 'Henrique Bastos',
-    author_email = 'henrique@bastos.net',
+    author='Henrique Bastos',
+    author_email='henrique@bastos.net',
 
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 
-    install_requires = ['setuptools', 'fixture'],
+    install_requires=['setuptools', 'fixture'],
 
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
-)
-
+    classifiers=['Development Status :: 4 - Beta',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Internet :: WWW/HTTP'])

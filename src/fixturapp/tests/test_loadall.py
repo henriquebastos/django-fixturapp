811b219515dc976c77a8329396b8a390dd6d7cc6
Henrique Bastos <henrique@bastos.net>
2009-11-02 11:51:41 -0200

    Adjusted readability issues on test_loadall.py

--- src/fixturapp/tests/test_loadall.py
+++ src/fixturapp/tests/test_loadall.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.core.management import load_command_class, call_command
 from django.core.management.base import NoArgsCommand
+
 from fixturapp.management.commands import fixturapp_loadall
-from dummyapp.models import Dummy
-from dummyapp.datasets import DummyData
+from fixturapp.tests.dummyapp.models import Dummy
+from fixturapp.tests.dummyapp.datasets import DummyData
 
 
 def create_command():
@@ -16,12 +17,12 @@ class FixturappLoadAllTests(TestCase):
         """Sucessfully load the command ``fixturapp_loadall`` from django
         project
         """
-        self.failUnless(isinstance(create_command(),
+        self.assertTrue(isinstance(create_command(),
                                    fixturapp_loadall.Command))
 
     def test_command_is_valid_django_command(self):
         """Asserts that fixturapp_loadall command is a valid Django command"""
-        self.failUnless(isinstance(create_command(), NoArgsCommand))
+        self.assertTrue(isinstance(create_command(), NoArgsCommand))
 
     def test_fixture_was_loaded_by_calling_command(self):
         """Check if Dummy fixtures were loaded to the database"""
